02036dd6e4ffebed1980fa4ac4a46b62bb9c4ae4
Shraddha Rao <shraddharaom@gmail.com>
2022-12-21 15:13:38 -0600

    commit tree 45c43049cf5e7622e226d55aae5f91071d99ada4

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({ stockArray, onAddStock }) {
+  const stockList = stockArray.map((stock) => (
+    <Stock key={stock.id} stock={stock} onStockClick={onAddStock} />
+  ));
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stockList}
     </div>
   );
 }
